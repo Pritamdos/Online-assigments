080889b964f4d5748fddcaf551cbac216883db91
sonipritam <pritam.kumar@venturepact.com>
2018-11-24 17:40:21 +0530

    changes done in generate-uni-list

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,15 +1,15 @@
 
-    $(document).ready(function () {
-        $(document).on("click", ".notif-btn", function () {
-            $(".notif-item").toggleClass('hide');
-        });
-
-        AOS.init();
-         $('.nav').nav();
-        $(".notif-flex .click").click(function() { 
-        $(".notif-flex").toggleClass('off');
-        });
-
+$(document).ready(function () {
+    $(document).on("click", ".notif-btn", function () {
+        $(".notif-item").toggleClass('hide');
     });
+
+   AOS.init();
+    $('.nav').nav();
+    $(".notif-flex .click").click(function () {
+        $(".notif-flex").toggleClass('off');
+    });
+
+});
 
 
