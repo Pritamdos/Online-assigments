3d062a25aab80ba432586c1ff96af028abdda39b
Pritam Soni <pritamsoni5555@gmail.com>
2018-11-25 21:43:07 +0530

    first commit

--- assets/js/style.js
+++ assets/js/style.js
@@ -9,6 +9,21 @@ $(document).ready(function () {
     $(".notif-flex .click").click(function () {
         $(".notif-flex").toggleClass('off');
     });
+    $("#slide-down").click(function () {
+        $(".notif-item").addClass('hide').addClass('slideInDown');
+    });
+    $('.lead-tstmnl-slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        variableWidth: true,
+        autoplay:true,
+        autoplaySpeed: 4000,
+        dots: true,
+        arrows: false
+      });
+      
+      
 
 });
 
