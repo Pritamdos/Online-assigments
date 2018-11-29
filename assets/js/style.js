6d6b1eedefb6ec611322520d2b7bd03c3c758476
sonipritam <pritam.kumar@venturepact.com>
2018-11-29 16:23:31 +0530

    changes done

--- assets/js/style.js
+++ assets/js/style.js
@@ -38,6 +38,20 @@ $(document).ready(function () {
         $(window).trigger('scroll');
 
     });
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.scroll_To_Top').fadeIn();
+        }
+        else {
+            $('.scroll_To_Top').fadeOut();
+        }
+    });
+
+
+    $('.scroll_To_Top').click(function () {
+        $('html, body').animate({ scrollTop: 0 }, 700);
+        return false;
+    });
 
 
 });
